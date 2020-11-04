0f5cdfac4057b9594765453c26b8b688417237ee
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-11-04 16:06:38 +0100

    starting coroutines sample

---
+++ idea_samples/src/main/kotlin/coroutines_start.kt
@@ -0,0 +1,25 @@
+// starting coroutines sample (compare with 
+import kotlinx.coroutines.*
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicInteger
+
+fun main() {
+	val latch = CountDownLatch(10_000)
+	val counter = AtomicInteger()
+
+	val start = System.currentTimeMillis()
+	for (i in 1..10_000) {
+		GlobalScope.launch {
+			counter.incrementAndGet()
+			delay(100)
+			counter.incrementAndGet()
+			latch.countDown()
+		}
+	}
+
+	latch.await(10, TimeUnit.SECONDS)
+
+	println("Executed ${counter.get()/2} coroutines in ${System.currentTimeMillis() - start}ms")
+	println("done!")
+}
