ca94e7977d56068dcd86befabd33028e2fbfdcf5
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-11-10 16:34:10 +0100

    coroutines: parent job sample

---
+++ idea_samples/src/main/kotlin/coroutines_parent_job.kt
@@ -0,0 +1,37 @@
+import kotlinx.coroutines.*
+import java.util.*
+
+// coroutine parent job sample (with parent job we can cancel all child jobs at once)
+fun main() {
+	val parentJob = Job()
+
+	List(10) {
+		GlobalScope.async(Dispatchers.Default + parentJob) {
+			produceBeautifulUuid()
+		}
+	}
+
+	runBlocking {
+		delay(100)
+		parentJob.cancel()
+		delay(1000)
+	}
+
+	println("done!")
+}
+
+suspend fun produceBeautifulUuid(): String {
+	try {
+		val uuids = List(1000) {
+			yield()
+			UUID.randomUUID()
+		}
+
+		println("coroutine done")
+		return uuids.sorted().first().toString()
+	} catch (t: CancellationException) {
+		println("got cancelled")
+	}
+
+	return ""
+}
