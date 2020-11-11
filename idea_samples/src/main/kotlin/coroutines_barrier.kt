c0e273fb59f12f1a0c4a01d4f33cb522058f36d1
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-11-11 14:32:48 +0100

    coroutines: barrier sample

---
+++ idea_samples/src/main/kotlin/coroutines_barrier.kt
@@ -0,0 +1,38 @@
+// barrier design pattern sample
+import kotlinx.coroutines.*
+import java.util.*
+
+// `FavoriteCharacter` represent barrier in our case
+data class FavoriteCharacter(val name: String, val catchphrase: String, val repeats: Int)
+
+
+fun main() {
+	runBlocking {
+		val character = FavoriteCharacter(
+			getName().await(), getCatchphrase().await(), getRepeats().await())
+
+		with(character) {
+			println("$name says: ${catchphrase.repeat(repeats)}")
+		}
+	}
+
+	println("done!")
+}
+
+fun getName() = GlobalScope.async {
+	delay(Random().nextInt(100).toLong())
+	println("Got name")
+	"Inigo Montoya"
+}
+
+fun getCatchphrase() = GlobalScope.async {
+	delay(Random().nextInt(100).toLong())
+	println("Got catchphrase")
+	"Hello. My name is Inigo Montoya. You killed my father. Prepare to die."
+}
+
+fun getRepeats() = GlobalScope.async {
+	delay(Random().nextInt(100).toLong())
+	println("Got repeats")
+	6
+}
